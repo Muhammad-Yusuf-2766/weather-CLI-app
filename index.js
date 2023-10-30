bd2a2d0a55b8bf6b20b446c7d9e3145fff6b664a
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2023-10-30 14:17:51 +0900

    #2. Prcess.args

---
+++ index.js
@@ -0,0 +1,22 @@
+const getArguments = require("./helpers/arguments");
+
+const startCLI = () => {
+    const args = getArguments(process.argv);
+    console.log(args);
+
+    if(args.h) {
+        // help
+    }
+
+    if(args.h) {
+        //help
+    }
+    
+    if(args.t) {
+        //save token
+
+        // result
+    }
+};       
+
+startCLI();
